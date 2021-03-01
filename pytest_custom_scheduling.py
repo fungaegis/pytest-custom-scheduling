e7c41ca9e0727a1ecec5a77527a5097a4b589afb
fungaegis <fungaegis@gmail.com>
2021-03-01 17:36:55 +0800

    add function

--- pytest_custom_scheduling.py
+++ pytest_custom_scheduling.py
@@ -11,7 +11,7 @@ from xdist.scheduler import (
 class LoadCustomNameScheduling(LoadScopeScheduling):
 
     def _split_scope(self, nodeid):
-        pattern = r"\${(\w*)}"
+        pattern = r"{(\w*)}"
         res = re.search(pattern, nodeid)
         if res:
             return res.group(1)
@@ -42,3 +42,13 @@ def pytest_xdist_make_scheduler(config, log):
         "loadfile": LoadFileScheduling
     }
     return schedulers[dist](config, log)
+
+
+def pytest_collection_modifyitems(items):
+    pattern = r"\${(\w*)}"
+    for item in items:
+        item.name = item.name.encode("utf-8").decode("unicode_escape")
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
+        if re.search(pattern, item.name):
+            item.name = item.name.replace("TestSmoke", "TestLogin")
+            item._nodeid = item._nodeid.replace("TestSmoke", "TestLogin")
